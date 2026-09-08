c349ddd77d12ff9b81d836066e189cb74d5fbee9
zuev1ilya11-afk <zuev1ilya11@gmail.com>
2026-09-09 01:18:31 +0300

    Update Google Apps Script deployment URL

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.BUSINESS_OS_CONFIG={VK_APP_ID:54758847,GAS_WEB_APP_URL:'https://script.google.com/macros/s/AKfycbxPZZmFuqa0Tdt7xtwha-O31CesRkLs7a3W36YUW1a00rUqhPUJQLnBInMlw-eAL_Ln/exec'};
+window.BUSINESS_OS_CONFIG={VK_APP_ID:54758847,GAS_WEB_APP_URL:'https://script.google.com/macros/s/AKfycbysoAJVkL4but9HNrERFUrN7tGf9bkTB7GnxT6SOnxeirRsCOfMT7jeaLKpRPUGnVo/exec'};
