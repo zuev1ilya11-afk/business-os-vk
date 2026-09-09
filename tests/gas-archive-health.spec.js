c4a85b099082db9d690d77a5cc60547d923f593f
zuev1ilya11-afk <zuev1ilya11@gmail.com>
2026-09-09 12:07:51 +0300

    Test corrected Apps Script deployment URL

--- tests/gas-archive-health.spec.js
+++ tests/gas-archive-health.spec.js
@@ -1,6 +1,6 @@
 const {test,expect}=require('@playwright/test');
 
-const GAS='https://script.google.com/macros/s/AKfycbx6l6V_jjZdbWQGlj0DcR4Uf4wvMc8hA24DuIzdmi-Ek76QH1mQS0tm3Q_Er1IsWEumzQ/exec';
+const GAS='https://script.google.com/macros/s/AKfycbx6l6V_jjZdbWQGlj0DcR4Uf4wvMc8hA24DuIzdmi-Ek76QH1mQS0tm3Q_Erl1sWEumzQ/exec';
 
 test('Google Apps Script archive deployment is reachable',async({request})=>{
   const r=await request.post(GAS,{form:{action:'ping'}});
